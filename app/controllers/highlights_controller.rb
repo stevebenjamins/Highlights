01aeef02944c61f1fe6e985b7f84f7edd4308578
Steve Benjamins <steve@stevebenjamins.com>
2012-02-12 20:20:06 -0600

    Graphical overhaul. Refactored code. Automatically shows yesterdays games.

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,5 +1,12 @@
 class HighlightsController < ApplicationController
   def index
+    require 'nokogiri' 
+    require 'open-uri' 
+    def Time.yesterday; now - 86400; end
+    @today = Time.new.yesterday
+    @x = 'http://www.nba.com/gameline/' + @today.strftime('%Y%m%d')
+    @doc = Nokogiri::HTML(open(@x))
+    
   end
   
   def iframe
